c4062407e15b1283eba4fe2b64b9f67cd5378a33
Arsenii Fomin <fomin.artmind@gmail.com>
2024-03-26 10:54:24 +0700

    Adding default items on container first opening/destroy without opening

--- EpicLoot/Container_Patch.cs
+++ EpicLoot/Container_Patch.cs
@@ -7,6 +7,17 @@ namespace EpicLoot
     [HarmonyPatch(typeof(Container), nameof(Container.AddDefaultItems))]
     public static class Container_AddDefaultItems_Patch
     {
+        // already created (on Awake call) default items should be removed first 
+        public static void Prefix(Container __instance)
+        {
+            if (__instance == null || __instance.m_piece == null)
+            {
+                return;
+            }
+
+            __instance.m_inventory.RemoveAll();
+        }
+
         public static void Postfix(Container __instance)
         {
             if (__instance == null || __instance.m_piece == null)
@@ -25,6 +36,45 @@ namespace EpicLoot
                     __instance.m_inventory.AddItem(item);
                     EpicLoot.Log($"  - {item.m_shared.m_name}" + (item.IsMagic() ? $": {string.Join(", ", item.GetMagicItem().Effects.Select(x => x.EffectType.ToString()))}" : ""));
                 }
+            }
+        }
+    }
+
+    // Looks like there is no need to change this since all containers are not empty initially
+    // [HarmonyPatch(typeof(Container), nameof(Container.GetHoverText))]
+
+    [HarmonyPatch(typeof(Container), nameof(Container.RPC_RequestOpen))]
+    public static class Container_RPC_RequestOpen_Patch
+    {
+        public static void Prefix(Container __instance, long uid, long playerID)
+        {
+            if (__instance == null || __instance.m_piece == null)
+            {
+                return;
+            }
+
+            if (__instance.m_nview.IsOwner() && !__instance.m_nview.GetZDO().GetBool("EL_container_items_rolled".GetStableHashCode()))
+            {
+                __instance.AddDefaultItems();
+                __instance.m_nview.GetZDO().Set("EL_container_items_rolled".GetStableHashCode(), value: true);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Container), nameof(Container.OnDestroyed))]
+    public static class Container_OnDestroyed_Patch
+    {
+        public static void Prefix(Container __instance)
+        {
+            if (__instance == null || __instance.m_piece == null)
+            {
+                return;
+            }
+
+            if (__instance.m_nview.IsOwner() && !__instance.m_nview.GetZDO().GetBool("EL_container_items_rolled".GetStableHashCode()))
+            {
+                __instance.AddDefaultItems();
+                __instance.m_nview.GetZDO().Set("EL_container_items_rolled".GetStableHashCode(), value: true);
             }
         }
     }
